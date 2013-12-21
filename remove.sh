45f206b6eebf3e83c4906959a23b14ac88ce1653
Rubén Caro <ruben.caro.estevez@gmail.com>
2013-12-21 14:31:08 +0100

    Initial dump

---
+++ remove.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+# plugin name
+PLUGIN_NAME="smart_highlight"
+APP_NAME="smart_highlight"
+# plugin install path for all user
+PLUGIN_PATH_ALL=/usr/lib/gedit/plugins/
+# plugin install path for current user
+PLUGIN_PATH_USER=~/.local/share/gedit/plugins/
+# plugin configuarion path for current user
+CONFIG_PATH_USER=~/.local/share/gedit/plugins
+# plugin locale path
+LOCALE_PATH=/usr/share/locale
+
+
+read -p "Are you sure you want to remove ${PLUGIN_NAME} completedly? Root privilege is necessary. (y/n) : " rm_flg
+
+if [ "${rm_flg}" == "Y" ] || [ "${rm_flg}" == "y" ]; then
+	# remove plugin
+	echo "Remove plugin files..."
+	rm -rf ${PLUGIN_PATH_USER}/${PLUGIN_NAME}*
+	sudo rm -rf ${PLUGIN_PATH_ALL}/${PLUGIN_NAME}*
+
+	echo "Remove locale files..."
+	sudo rm ${LOCALE_PATH}/*/LC_MESSAGES/"${APP_NAME}.mo"
+
+	echo "Remove configuration files..."
+	rm -rf ${CONFIG_PATH_USER}/${APP_NAME}
+
+	echo "Plugin is removed successfully."
+else
+	echo "Remove action is canceled."
+fi
+
+
